488b167a66c406b6b1fbfa273caaa9ff4c0baa7a
DevinFafara <devinfafara@gmail.com>
2025-02-25 20:34:27 +0100

    test var env 2

--- netlify/functions/check-password.js
+++ netlify/functions/check-password.js
@@ -14,7 +14,7 @@ exports.handler = async (event) => {
     const { password } = JSON.parse(event.body);
     const correctPassword = process.env.REACT_APP_PASSWORD;
   
-    return {
+    return { 
       statusCode: password === correctPassword ? 200 : 401,
       headers: {
         "Access-Control-Allow-Origin": "*",
